f4cab7c4dc5b60197edc72541e4d6f1985ee5ac3
muhammadraza <mr.muhammad.raza86@gmail.com>
2016-04-17 22:31:57 +0100

    done some work around task

--- index.js
+++ index.js
@@ -1,11 +1,73 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-var HelloWorld = React.createClass({
-	render: () => {
-		return <div>
-			<h1>Hello World</h1>
-		</div>;
+var Header = React.createClass({
+	render: function() {
+		return (<div className="header">
+			This is a header			
+		</div>);		
 	}
 });
-ReactDOM.render(<HelloWorld />, document.getElementById('app'));
+
+var Footer = React.createClass({
+	render: function() {
+		return (<div className="footer">
+			This is a footer			
+		</div>);		
+	}
+});
+
+var Main = React.createClass({	
+	render: function() {
+		return (<div className="body">	
+		</div>);		
+	}
+});
+
+var Note = React.createClass({	
+	remove: function(){
+		this.refs.note.getDOMNode
+	},
+	render: function() {
+		return (<div className="note">
+			<div>This is a Note</div>
+			<div ref="note" onClick={this.remove} className="delete"></div>			
+		</div>);		
+	}
+});
+
+var Board = React.createClass({	
+	getInitialState: function() {
+		return {count: 4}
+	},
+	getNotes: function(){
+		var notes = [];
+		for (var i=0; i < this.state.count; i++) {
+			notes.push(<Note key={i}/>);        	
+    	} 
+    	return notes;
+		
+	},
+	render: function() {
+		return (<div className="board">		
+			{this.getNotes().map(function(note){
+				return note
+			})}            		 		
+		</div>);		
+	}
+});
+
+
+var App = React.createClass({
+	render: function() {
+		return <div>
+			<Header />
+			{this.props.children}
+			<Footer />
+			</div>;		
+	}
+});
+
+
+
+ReactDOM.render(<App><Board /></App>, document.getElementById('app'));
